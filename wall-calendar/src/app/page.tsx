73bab65eb7ed4e2c5b286223983c5268ab682241
Gayathri Ponnaganti <ponnagantigaya3@gmail.com>
2026-04-09 21:19:22 +0530

    Delete wall-calendar directory

--- wall-calendar/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WallCalendar from '@/components/WallCalendar'
-
-export default function Home() {
-  return (
-    <main style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '1.5rem' }}>
-      <WallCalendar />
-    </main>
-  )
-}
